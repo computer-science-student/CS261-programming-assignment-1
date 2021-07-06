e9a90c7997e91c8bd2215c81ebdb06a2ded159d4
computer-science-student <legorrej@oregonstate.edu>
2021-07-06 15:20:25 -0500

    I am commenting the code in k.c with the code I have in dynamicarray.c I got down to the delete function in dynamic array. I haven't added my code or comments to delete yet.

--- k.c
+++ k.c
@@ -1,23 +1,33 @@
+/*
+    Insert programmer names and id here for credit.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 
-
+// Struct for students.
 struct student{
 	char* name;
 	int points;
 };
 
-
+// Struct for the dynamic array
 struct dynArray {
 	int size;
 	int capacity;
+	// The data pointer pointer will
+	// be an array with pointers. Each
+	// of those pointers will point to
+	// a different student structure,
+	// which is stored in the heap.
 	void** data;
 };
 
-
+// Define global struct pointer
+// for the dynamic array.
 struct dynArray* D;
 
+// Function prototypes.
 void add(void*);
 void* get(int);
 void set(void*, int);
@@ -32,8 +42,12 @@ void disp();
 
 int main() {
 	D = malloc(sizeof(struct dynArray));
+	// Assert functions check if the condition is null or
+	// not, false or not. If the condition is null or false,
+	// the program terminates with error message. If the
+	// conditional is not null or false, the program continues.
 	assert(D);
- 	init(4);
+ 	init(4); // Init is used to create the array.
 	test();
 	cleanup();
 	disp();
@@ -41,12 +55,12 @@ int main() {
 
 	return 0;
 }
-
-
+// Initialize dynamic array. This is like a constructor.
 void init(int c) {
 	assert(c);
 	D->capacity = c;
 	D->size = 0;
+	// Does this malloc need to be typecasted?
 	D->data = malloc(D->capacity * sizeof(struct student*));
 }
 
@@ -76,6 +90,7 @@ void set(void* value, int index) {
 
 void delete(int index) {
 	assert(index >= 0);
+
 	assert(index < D->size);
 	int i;
 	for (i=index+1; i<D->size; i++) {
@@ -88,7 +103,7 @@ void delete(int index) {
 void add(void* record) {
 	if (D->size == D->capacity) {
 		_resize(D->capacity*2);
-	}	
+	}
 	D->data[D->size] = record;
 	D->size++;
 }
@@ -127,11 +142,11 @@ void disp() {
 	int i;
 
 	struct student *ptr;
-	
+
 	printf("capacity: %d\n", D->capacity);
 	printf("size: %d\n", D->size);
 	for (i=0; i<D->size; i++) {
-		ptr = D->data[i];		
+		ptr = D->data[i];
 		printf("Student %d: ", i);
 		printf("%s, ", ptr->name);
 		printf("%d\n", ptr->points);
@@ -164,7 +179,7 @@ void test() {
 	student3 = (struct student *)malloc(sizeof(struct student));
 	student3->name = "Khanh";
 	student3->points = 99;
-	
+
 	student4 = (struct student *)malloc(sizeof(struct student));
 	student4->name = "Kristine";
 	student4->points = 33;
@@ -176,11 +191,11 @@ void test() {
 	student6 = (struct student *)malloc(sizeof(struct student));
 	student6->name = "Trent";
 	student6->points = 295;
-	
+
 	student7 = (struct student *)malloc(sizeof(struct student));
 	student7->name = "Xi";
-	student7->points = 55;	
-	
+	student7->points = 55;
+
 	student8 = (struct student *)malloc(sizeof(struct student));
 	student8->name = "Bill";
 	student8->points = 1;
@@ -188,7 +203,7 @@ void test() {
 	student9 = (struct student *)malloc(sizeof(struct student));
 	student9->name = "Sarah";
 	student9->points = 15;
-	
+
 	student10 = (struct student *)malloc(sizeof(struct student));
 	student10->name = "Teresa";
 	student10->points = 92;
@@ -209,8 +224,8 @@ void test() {
 		printf("Get points: %d\n", ptr->points);
 	putchar('\n');
 
-	free(student1); 
-	free(student2); 
+	free(student1);
+	free(student2);
 	free(student3);
 	free(student4);
 	free(student5);
