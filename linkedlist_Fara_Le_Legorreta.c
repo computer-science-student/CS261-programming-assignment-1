2d07050a37aebad1c6e9afc89df1ba8ac8b3e03b
computer-science-student <legorrej@oregonstate.edu>
2021-07-05 21:42:56 -0500

    Update linkedlist_Fara_Le_Legorreta.c
    I replaced my code with Le.

--- linkedlist_Fara_Le_Legorreta.c
+++ linkedlist_Fara_Le_Legorreta.c
@@ -1,153 +1,133 @@
+// Online C compiler to run C program online
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 
-struct Node {
-    struct Node* prev;
-    int data;
-    //struct Node* link; // This was used for the singly linked list
+struct Node{//Node of a doubly linked list
     struct Node* next;
+    int data;
+    struct Node* prev;
 };
 
+//declare global value
 struct Node* head;
 struct Node* tail;
 
-// Function protoypes
-void insert(struct Node*, struct Node*);
-void delete(struct Node*, struct Node*);
+void insert(struct Node*, struct Node*, struct Node*);
+void delete(struct Node*, struct Node*, struct Node*);
 void _insert1(struct Node*);
-void _insert2(struct Node*, struct Node*);
+void _insert2(struct Node*, struct Node*, struct Node*);
 void _delete1();
-void _delete2(struct Node*, struct Node*);
+void _delete2(struct Node*, struct Node*, struct Node*);
 void _delete3(struct Node*, struct Node*);
-
 void init();
 void cleanup();
-
 void disp();
 void test();
 
-
-int main() {
+int main(){
     init();
     test();
     cleanup();
     return 0;
 }
 
-void init(){
-    head = tail = NULL;
+void init(){ //create 2 pointers to start the doubly linked list
+    head= tail = NULL;
 }
 
-void cleanup() {
-    struct Node* temp;
-    while (head != NULL) {
-        temp = head;
-        head = head->next;
-        free(temp);
+void cleanup(){
+    struct Node* curr;//create a curr pointer to go thru all the node in the doubly linked list
+    while(head != NULL){
+        curr->prev = head; // assign the temp pointer to the prev pointer first node
+        curr = curr->next;
+        free(curr);
     }
 }
 
-void _insert1(struct Node* curr) {
-    if (head == NULL) {
-		curr->link = NULL;
-		head = tail = curr;
-	}
-	else {
-		curr->link = head;
-		head = curr;
-	}
+void _insert1(struct Node* curr){//insert node at the front
+        if(head == NULL){//if there is no node at the linked list yet
+            curr->next = NULL; //make sure the end of the added node is NULL
+            curr->next = NULL;
+            head = tail = curr->prev ;
+        }
+        else{//if the doubly linked list already have existing mode
+            curr->prev = NULL;
+            head = curr->prev;
+        }
 }
-
-
-void _insert2(struct Node* curr, struct Node* pred) {
-    if (pred->link == NULL) {
-		curr->link = NULL;
-		pred->link = curr;
-		tail = curr;
-	}
-	else {
-		curr->link = pred->link;
-		pred->link = curr;
-	}
-}
-
-
-void insert(struct Node* curr, struct Node* pred) {
-    if (pred == NULL) {
-        _insert1(curr);
+// I MIGHT CUT DOWN INSERT FRONT, END AND MID AND BREAK DOWN 3 INSERT FUNCTION IF NEEDED
+void _insert2(struct Node* curr, struct Node *pred, struct Node* adjc){//adjc is the node next to current node
+    if(pred->next == NULL){//if added at the end of the list
+      curr->next= NULL;
+      pred->next = curr->prev;
+      curr->prev = pred->next; //make them next of previous node and prev of crueent node point to each other
+      tail = curr->prev;
     }
-    else {
-        _insert2(curr, pred);
+    else{//if insert at middle
+      curr->next = pred->next; //make next of current point to the same place as previous node
+      //connect the pointers of curr and pred node together
+      pred->next = curr->prev;
+      pred->next = curr->prev;
+      //connect the pointers of curr and adjc together
+      curr->next= adjc->prev;
+      adjc->prev= curr->next;
     }
 }
-
-
-void _delete1() {
-    struct Node* curr = head;
-	if (curr->link == NULL) {
-		head = tail = NULL;
-	}
-	else {
-		head = curr->link;
-	}
-	free(curr);
+void insert(struct Node* curr, struct Node* pred, struct Node* adjc){
+  if(pred == NULL){
+  _insert1(curr);
+  }
+  else{
+  _insert2(curr, pred, adjc);
+  }
 }
 
+void _delete1(){//delete first node
+  struct Node* curr; //create a node pointer pointing to the 1st node of the list
+  curr->prev = head;
+  if(curr->next = NULL){//if there is only 1 node
+    head = tail = NULL; //reset to inital condition
+  }
+  else{//more than 1 deleted node
+  head = curr->next; //make the head pointer point to the same place as the next pointer of the delete node
+  }
+  free(curr);//delete the node
+}
 
-void _delete2(struct Node* curr, struct Node* pred) {
-    pred->link = curr->link;
+void _delete2(struct Node* curr, struct Node* pred, struct Node* adjc){//delete at middle
+    pred->next = curr-> next; //make the next of previous node points to the same place as the deletemed node
+    adjc->prev = pred->next; //make the prev of the node next to the delete node points to the node preiouvs to delete node
     free(curr);
 }
 
-
-void _delete3(struct Node* curr, struct Node* pred) {
-    pred->link = NULL;
-	tail = pred;
-	free(curr);
+void _delete3(struct Node* curr, struct Node* pred){//delete at end
+    pred->next = NULL;
+    tail = pred->prev; //make tail point to previous node
+    free(curr);
 }
 
-
-void delete(struct Node* curr, struct Node* pred) {
-    if (pred == NULL) {
-		_delete1();
-	}
-	else if (curr->link == NULL) {
-		_delete3(curr, pred);
-	}
-	else {
-		_delete2(curr, pred);
-	}
+void delete(struct Node* curr, struct Node* pred, struct Node* adjc){
+    if( pred == NULL){
+        _delete1();
+     }
+     else if( curr->next == NULL){
+        _delete3(curr, pred);
+     }
+     else{
+        _delete2(curr, pred, adjc);
+     }
 }
 
-
-void disp() {
-    struct Node* curr = head;
-    while (curr != NULL) {
+void disp(){
+    struct Node*curr;
+    curr->prev= head;//make prev point of curr node points to head
+    while(curr != NULL){//as long as the list doesn't ends
         printf("%d\t", curr->data);
-        curr = curr->link;
+        curr = curr->next; //move on to the next node
     }
     printf("\n");
 }
-
-
-void test() {
-    //printf("This is from linked list!\n");
-    struct Node* n1 = malloc(sizeof(struct Node));
-    n1->data = 10; n1->link = NULL; insert(n1, NULL); disp();
-    struct Node* n2 = malloc(sizeof(struct Node));
-    n2->data = 20; n2->link = NULL; insert(n2, n1); disp();
-    struct Node* n3 = malloc(sizeof(struct Node));
-    n3->data = 30; n3->link = NULL; insert(n3, n2); disp();
-    struct Node* n4 = malloc(sizeof(struct Node));
-    n4->data = 25; n4->link = NULL; insert(n4, n2); disp();
-    struct Node* n5 = malloc(sizeof(struct Node));
-    n5->data = 40; n5->link = NULL; insert(n5, n3); disp();
-    delete(n1, NULL); disp();
-    n1 = malloc(sizeof(struct Node));
-    n1->data = 10; n1->link = NULL; insert(n1, NULL); disp();
-    delete(n2, n1); disp();
-    n2 = malloc(sizeof(struct Node));
-    n2->data = 20; n2->link = NULL; insert(n2, n1); disp();
-    delete(n5, n3); disp();
+// I'LL WORK ON THIS FUNCTION LATER AND THEN COMPILE IT TO SEE IF MY LOGIC IS RIGHT
+void test(){
 }
